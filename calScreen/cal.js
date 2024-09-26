314fc19511195efa93b8a1d98436523af1e23ec9
Pradip Tapaniya <pradiptapaniya2023.dev@gmail.com>
2024-09-26 22:23:54 +0530

    cal_add

---
+++ calScreen/cal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Calrn = () => {
+    return(
+        <View style={{flex:1}}>
+            <Text style={{fontSize:30}}>
+                Hello Calc
+            </Text>
+        </View>
+    );
+}
+
+export default Calrn;
